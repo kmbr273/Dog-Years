19e600616d021ce4d27a9168caac5ce806094fd7
Karlie <88041216+kmbr273@users.noreply.github.com>
2021-10-07 13:06:24 -0500

    First Commit

---
+++ dogyears.js
@@ -0,0 +1,15 @@
+const myAge = 70;
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears += laterYears; 
+
+const myName ='Karlie Brown'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
